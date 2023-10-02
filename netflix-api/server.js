63cbfe7bda5a4102a84525eee278106654e9087d
YOKESHDHANABALAN <yokeshdhanabalan@gmail.com>
2023-10-02 15:30:39 +0530

    commit

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/netflix", {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
