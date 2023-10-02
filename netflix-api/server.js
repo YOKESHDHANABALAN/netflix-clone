a1c7a6c63afabe8aeea031f5af113175b8e655b0
Yokesh D <107738665+YOKESHDHANABALAN@users.noreply.github.com>
2023-10-02 13:18:30 +0530

    Update server.js

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/netflix", {
+  .connect("mongodb://localhost:27017/netflix", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
